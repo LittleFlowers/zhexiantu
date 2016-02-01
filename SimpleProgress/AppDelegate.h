b750393d910d2f20a0047c8647c7e76aa118b3d2
apple <lingmei_zhaoyi@caisedayinji.local>
2016-02-01 14:27:18 +0800

    Initial Commit

---
+++ SimpleProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleProgress
+//
+//  Created by 赵诣 on 16/2/1.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
